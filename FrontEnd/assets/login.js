23812a202b1421e1a49514e5a5d7718457da9e39
angieleutard <leutardangie@mbp-de-leutard.home>
2022-12-27 12:05:15 +0100

    add changes

---
+++ FrontEnd/assets/login.js
@@ -0,0 +1,30 @@
+const form = document.getElementById("logIn");
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const email = document.getElementById('email').value
+    const password = document.getElementById('pass').value
+
+    const data = {
+        email: email, 
+        password: password
+    }
+
+    fetch("http://localhost:5678/api/users/login", {
+        method: 'POST',
+        headers: {
+        'Content-Type': 'application/json;charset=utf-8'
+        },
+        body: JSON.stringify(data)
+    }).then(function(res) {
+        console.log(res)
+        if (res.ok) {
+            console.log("Utilisateur connecté!");
+            document.location.href="./index.html"; 
+            } else {
+            console.log("Alerte, intrus!");
+            let errorText = document.querySelector(".errorMessage");
+            errorText.innerHTML = "Utilisateur inconnu !"
+            } 
+            return res;
+    })     
+})
